62b0941eb5036ee1810bd42d52414459a4de6e32
bajrang <251160412+bajrangMessesThingsUp@users.noreply.github.com>
2026-09-15 14:29:10 +0530

    add one hot encoder

--- ml_lib/feature_engineering/encoding.py
+++ ml_lib/feature_engineering/encoding.py
@@ -25,3 +25,20 @@ class LabelEncoder():
         for i in self.categories:
                 dataset[dataset==i] = self.categories.index(i)
         return dataset.to_numpy() 
+
+class onehotencoder():
+    def __init__(self):
+        self.categories = []
+    def fit(self, dataset):
+      for i in range(len(dataset.columns)):
+        self.categories.append(dataset[dataset.columns[i]].unique())
+    def transform(self,dataset):
+      cat_list=[]
+      for i in range(len(self.categories)):
+        for j in self.categories[i]:
+          cat_list.append(j)
+      new_df = pd.DataFrame(columns=cat_list)
+      for i in range(len(self.categories)):
+        for j in self.categories[i]:
+          new_df[j] = (dataset.iloc[:,i]==j).astype(int)
+      return new_df
