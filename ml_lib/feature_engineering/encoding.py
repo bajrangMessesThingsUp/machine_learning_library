491fe8b2618d5c8c7247c9bc4f7e042e67f62b6c
bajrang <251160412+bajrangMessesThingsUp@users.noreply.github.com>
2026-09-15 12:26:25 +0530

    add label encoder and improve ordinal encoder for pipeline

--- ml_lib/feature_engineering/encoding.py
+++ ml_lib/feature_engineering/encoding.py
@@ -5,7 +5,23 @@ class OridnalEncoder():
   def __init__(self,categories: list[list]):
     self.categories = categories
   def fit(self,dataset):
+    for i in range(dataset.shape[1]):
+        if(sum(~dataset.iloc[:,i].isin(self.categories[i]))!=0):
+            raise ValueError
+  def transform(self,dataset):
     for i in self.categories:
       for j in i:
         dataset[dataset==j] = i.index(j)
-    return dataset 
+    return dataset.to_numpy() 
+
+class LabelEncoder():
+    def __init__(self):
+        self.categories = []
+    
+    def fit(self, dataset):
+        for i in dataset.unique():
+            self.categories.append(i)
+    def transform(self,dataset):
+        for i in self.categories:
+                dataset[dataset==i] = self.categories.index(i)
+        return dataset.to_numpy() 
